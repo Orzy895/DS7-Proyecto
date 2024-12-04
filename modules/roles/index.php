287f1a09fbd80d85ba9839c154221ac6d3f2c986
byakuDNY <r13luo@gmail.com>
2024-12-04 10:43:22 -0500

    deleted useless files

--- modules/roles/index.php
+++ modules/roles/index.php
@@ -5,7 +5,6 @@ include_once '../../template/head_template.php';
 <div class="flex flex-col justify-center items-center">
     <a href="views/roles.php" class="btn">Roles</a>
     <a href="/ds7-Proyecto/views/userAdmin.php" class="btn">Panel de Administración de Usuarios</a>
-    <a href="views/admin-roles.php" class="btn">Panel de Administración de Roles</a>
 </div>
 
 <?php
